268257a63106aa778f4743e5b127c1275c532476
Rifat Hasan <rh44672@co.champaign.il.us>
2024-02-13 14:53:03 -0600

    test-state

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,26 @@ import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import  TabButton  from './components/TabButton.jsx';
-
+import {EXAMPLES} from './data.js'
 
 function App() {
-  const [selectedTopic, setSelectedTopic]=useState('Please click a button');
+  const [selectedTopic, setSelectedTopic]=useState();
   //let tabContent='Please click a button'
   function handleSelect(selectedButton){
     setSelectedTopic(selectedButton)
     // tabContent=selectedButton;
     
+  }
+
+  let tabContent=<p>Please select a topic.</p>
+  if (selectedTopic){
+    tabContent=(<div id='tab-content'>
+    <h3>{EXAMPLES[selectedTopic].title}</h3>
+    <p>{EXAMPLES[selectedTopic].description}</p>
+    <pre>
+      <code>{EXAMPLES[selectedTopic].code}</code>
+    </pre>
+  </div>);
   }
   return (
     <div>
@@ -21,22 +32,20 @@ function App() {
       <h2>Core COncepts</h2>
       
       <ul>
-        <CoreConcept {...CORE_CONCEPTS[0]}/>
-        <CoreConcept {...CORE_CONCEPTS[1]}/>
-        <CoreConcept {...CORE_CONCEPTS[2]}/>
-        <CoreConcept {...CORE_CONCEPTS[3]}/>
+        {CORE_CONCEPTS.map((conceptItem)=><CoreConcept {...conceptItem}/>)}
       </ul>
       </section>
 
       <section id="examples">
         <h2>Examples</h2>
         <menu>
-          <TabButton onSelect={()=>handleSelect('Components')}>Components</TabButton>
-          <TabButton onSelect={()=>handleSelect('JSX')}>JSX</TabButton>
-          <TabButton onSelect={()=>handleSelect('Props')}>Props</TabButton>
-          <TabButton onSelect={()=>handleSelect('State')}>State</TabButton>
+          <TabButton isSelected={selectedTopic ==='components'} onSelect={()=>handleSelect('components')}>Components</TabButton>
+          <TabButton isSelected={selectedTopic ==='jsx'}onSelect={()=>handleSelect('jsx')}>JSX</TabButton>
+          <TabButton isSelected={selectedTopic ==='props'} onSelect={()=>handleSelect('props')}>Props</TabButton>
+          <TabButton isSelected={selectedTopic ==='state'} onSelect={()=>handleSelect('state')}>State</TabButton>
         </menu>
-        {selectedTopic}
+        {tabContent}
+        
       </section>
       </main>
     </div>
@@ -44,3 +53,4 @@ function App() {
 }
 
 export default App;
+
